96c9c209b28c005114d2b070c89bea339029d2e8
Przemyslaw Wiktorowski <2silverghost2@gmail.com>
2021-04-16 20:51:49 +0200

    Initial commit

---
+++ src/com/company/Operators.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        Operators operators = new Operators();
+        operators.additioton();
+
+    }
+
+
+    private void additioton(){
+
+        int a =1;
+        int b =2 + a;
+        int c = a + b;
+
+        System.out.println(c);
+
+
+
+    }
+
+}
